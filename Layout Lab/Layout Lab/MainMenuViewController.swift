95376416560e65f6a506b4814eb5039e09298c12
Patrick <pmcgonigle@gmail.com>
2022-05-15 11:30:54 -0500

    Initial main collection view runs

--- Layout Lab/Layout Lab/MainMenuViewController.swift
+++ Layout Lab/Layout Lab/MainMenuViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class MainMenuViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    	
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+	    }
 
 }
 
+extension MainMenuViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+}
+
+extension MainMenuViewController: UICollectionViewDelegate {
+    
+}
+	
