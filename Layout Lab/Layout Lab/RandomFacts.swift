b73c59582db15b6974bb2bca71290a52555bdcc3
Patrick <pmcgonigle@gmail.com>
2022-05-15 11:08:34 -0500

    Added random facts generator

---
+++ Layout Lab/Layout Lab/RandomFacts.swift
@@ -0,0 +1,81 @@
+//
+//  RandomFacts.swift
+//  Layout Lab
+//
+//  Created by Patrick McGonigle on 5/15/22.
+//
+
+import Foundation
+
+/// A local service that provides random facts to display.
+protocol RandomFacts {
+    
+    /// Gets a random fact
+    var randomFact: String { get }
+    
+    /// Tells how many facts there are
+    var count: Int { get }
+}
+
+/// Implementation of `RandomFacts` that keeps all data in memory
+class RandomFactsFromFile: RandomFacts {
+    
+    enum RandomFactsError: Error {
+        case badFile
+    }
+    
+    /// Create an instance
+    init() {
+        self.load()
+    }
+    
+    // MARK: RandomFacts
+    
+    var randomFact: String {
+        switch self.state {
+        case .ok(let facts): return facts.randomElement() ?? "???"
+        case .initial: return "<Not initialized>"
+        case .failed(let error): return "<Error: \(error.localizedDescription)>"
+        }
+    }
+    
+    var count: Int {
+        switch self.state {
+        case .ok(let facts): return facts.count
+        case .initial, .failed: return 0
+        }
+    }
+    
+    // MARK: Internal
+    
+    private var state: State = .initial
+    private var facts: [String] = []
+    
+    /// Internal state.  Playing around with this idea instead of throwing, etc. on bad file.
+    private enum State {
+        case initial
+        case ok(facts: [String])
+        case failed(error: Error)
+    }
+    
+    /// Loads all the random facts form a file
+    private func load() {
+
+        // Keeping this as stupid simple as possible to use, so I can focus on layouts,
+        // so don't even parameterize the file path.
+        let partialPath = NSString.path(withComponents: ["Random Facts", "facts"])
+        guard let fullPath = Bundle.main.path(forResource: partialPath, ofType: "txt") else {
+            self.state = .failed(error: RandomFactsError.badFile)
+            return
+        }
+        
+        do {
+            let contents = try String.init(contentsOfFile: fullPath)
+            let facts = contents.split(separator: "\n").map { String($0) }
+            self.state = .ok(facts: facts)
+        } catch {
+            self.state = .failed(error: error)
+        }
+    }
+    
+}
--- Layout Lab/Layout LabTests/Layout_LabTests.swift
+++ Layout Lab/Layout LabTests/Layout_LabTests.swift
@@ -8,29 +8,18 @@
 import XCTest
 @testable import Layout_Lab
 
-class Layout_LabTests: XCTestCase {
+class Layout_LabRandomFactsTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    private let randomFacts: RandomFacts = RandomFactsFromFile()
+
+    func testRandomFactsCount() throws {
+        XCTAssertEqual(self.randomFacts.count, 3090)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testRandomFact() throws {
+        XCTAssert(self.randomFacts.randomFact.count > 0)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    // Note that I can't even see or test the .initial and .failed states. 🤷🏻‍♂️
+    // A bad file just comes out in the `randomFact` call.  Is that what I want?
 }
